4d1d05e2c863fd1a495bf72519d381667057b959
Casey Hsu <Casey-Hsu@outlook.com>
2023-02-25 09:00:20 -0500

    "Second Change"

--- Controllers/AuctionsController.cs
+++ Controllers/AuctionsController.cs
@@ -58,6 +58,7 @@ namespace assignment_one.Controllers
         {
             if (ModelState.IsValid)
             {
+                auction.Id = User.Identity.GetHashCode();
                 _context.Add(auction);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
